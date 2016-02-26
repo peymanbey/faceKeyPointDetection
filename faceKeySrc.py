c71fb489be60ce6926cee530a7915d7a83896f41
Peyman Beyranvand <p.beyranvand@gmail.com>
2016-02-26 10:13:39 +0200

    MLP working tested

--- faceKeySrc.py
+++ faceKeySrc.py
@@ -154,6 +154,146 @@ class linear_regresion(object):
                 ('y',y.type, 'y_pred', self.y_pred.type)
             )
         return T.mean(T.neq(self.y_pred,y))
+       
+class HiddenLayer(object):
+    def __init__(self,
+                 rng,
+                 input,
+                 n_in,
+                 n_out,
+                 W=None,
+                 b=None,
+                 activation=T.tanh):
+        """units are fully connected
+        sigmoidial activation functin 
+        shape of W : (n_in,n_out)
+        shape of b : (n_out,)
+        
+        :type rng: numpy.random.RandomState
+        :param rng: random number generator for initializing W
+        
+        :type input: theano.tensor.dmatrix
+        :param input:symbolic tensor of the shape (n_examples, n_input)
+        
+        :type n_in: int
+        :param n_in: dimensionality of input
+        
+        :type n_out: int
+        :param n_out: number of hidden units
+        
+        :type activation: theano.Op or function
+        param activation: Non-linearity of the hidden units
+        
+        """
+        
+        #keep track of input
+        self.input = input
+        
+        #initializing network weight
+        
+        #W uniformely sampled from -sqrt(6./(n_in+n_hidden)) and 
+        # sqrt(6./(n_in+n_hidden)) for tanh function
+        if W is None:
+            W_values = numpy.array(
+                rng.uniform(
+                    low = -numpy.sqrt(6. / (n_in + n_out)),
+                    high = numpy.sqrt(6. / (n_in + n_out)),
+                    size = (n_in , n_out)
+                ),
+                dtype = theano.config.floatX
+            )
+            
+            if activation == theano.tensor.nnet.sigmoid: # for sigmoid function
+                W_values *= 4
+                
+            W = theano.shared( value=W_values, name='W' , borrow = True)
+            
+        if b is None:
+            b_values = numpy.zeros((n_out, ), dtype = theano.config.floatX)
+            b = theano.shared(value = b_values, name = 'b' , borrow = True)
+
+        self.W = W
+        self.b = b
+
+        lin_output = T.dot(input, self.W) + self.b
+        self.output = (
+            lin_output if activation is None
+            else activation(lin_output)
+        )        
+        # parameters of the model 
+        self.param = [self.W, self.b]
+
+class MLP(object):
+    """Multi-layer perceptron class
+    
+    Feed-forward neural network with one or more hidden layers
+    and non-linear activation defined by hiddenLayer and 
+    the output layer defined by linear_regresion
+    """
+    
+    def __init__(self,
+                 rng,
+                 input,
+                 n_in,
+                 n_hidden,
+                 n_out):
+        """initializing a MLP
+        
+        :type rng: numpy.random.RandomState
+        :param rng: random number generator for initializing MLP weights
+        
+        :type input: theano.tensor.TensorType
+        :param input: symbolic representation of the input samples
+        
+        :type n_in: int 
+        :pram n_in:  number of input units, dimension of input space
+        
+        :type n_hidden: int
+        :param n_hidden:number of hidden units
+        
+        :type n_out: int
+        :param n_out:number of output units
+        
+        """
+        
+        # for now we make the assumption that we have a single layer MLP
+        # activation functions tanh or sigmoid 
+        # output layer is multi-target regression defined in linear_regresion
+        
+        self.hiddenLayer = HiddenLayer(rng = rng,
+                                       input = input,
+                                       n_in = n_in,
+                                       n_out = n_hidden,
+                                       activation=T.tanh)
+        
+        # linear regression layer takes the output of the hidden layer
+        self.regression = linear_regresion( input = self.hiddenLayer.output,
+                                           n_in = n_hidden,
+                                           n_out = n_out)
+        
+        # define the L1 norm for regularization 
+        self.L1= (
+        abs(self.hiddenLayer.W).sum()
+            + abs(self.regression.W).sum()
+            )
+        # define the L2 regularization term 
+        self.L2= (
+        abs(self.hiddenLayer.W**2).sum()
+            + abs(self.regression.W**2).sum()
+            )
+        #the loss of the MLP is given by the MSE  loss of the regression layer
+        self.MSE= (
+            self.regression.loss_MSE    
+            )
+        # computing the number of error? 
+        # this is for accuracy calculation not necessary for regression
+        
+        # parameters of the model
+        self.param  = self.hiddenLayer.param + self.regression.param
+        
+        # kepp track of the input
+        self.input = input 
+        
         
 def train_early_stopping(train_model,
                          validate_model,
